82a3cd8f204cb5e9ea14431212230e1b8b721393
Igor Contri <igorcontri@gmail.com>
2025-11-16 13:53:55 -0300

    atualiza logos

--- projeto-bac-si/app/Models/CartItem.php
+++ projeto-bac-si/app/Models/CartItem.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     use HasFactory;
+
+    /**
+     * Os atributos que podem ser atribuídos em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity',
+    ];
 }
